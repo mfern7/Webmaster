22fdb701a93924160c8e5f35f55e0f40b178f899
Marcos <marcosf9@hotmail.com>
2019-11-02 12:45:09 -0300

    Clase 25_ php

---
+++ clase25/archivo.php
@@ -0,0 +1,20 @@
+<H1> Titulo principal</H1>
+
+<?php
+// comentario de una linea
+/* Etiquetas php <?php  ?> 
+
+ubicaciones de arch php
+XAMP: c/XAMP/htdocs/carpeta
+WAMP: c/WAMP/www/carpeta
+
+http://localhost/carpeta/archivo.php
+
+http://localhost/marcos_01/archivo.php
+
+
+*/
+
+echo "Hola Mundo";
+
+?>
