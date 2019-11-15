0317a04af53a4d73ffe192c912765245c7b0d6a1
Marcos <marcosf9@hotmail.com>
2019-11-14 21:58:19 -0300

    clase 30 corregido

--- clase29/libreria/interno.php
+++ clase29/libreria/interno.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+if (isset($_SESSION['id']) === false) {
+	header("location: index.php");
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 	<head>
--- clase29/libreria/login_pr.php
+++ clase29/libreria/login_pr.php
@@ -1,4 +1,49 @@
 <?php
 	// login_pr.php
 	
+	require('conexion.php');
+
+	$usuario = $_POST['usuario'];
+
+	$clave = md5($_POST['clave']);
+
+//2. Creamos la query
+
+$preguntar = "SELECT * FROM usuarios WHERE  usuario = '$usuario' AND clave = '$clave'";
+
+
+//3.ejecuto la query
+
+$p_ej = mysqli_query($conexion, $preguntar);
+
+if ($p_ej === false) {
+	echo "Error, ver SQL: $preguntar";
+} else {
+
+	if(mysqli_num_rows($p_ej) === 1) {
+
+		echo "los datos son correctos";
+
+		//Buscar el id de este usuario
+		$reg = mysqli_fetch_array($p_ej);
+		echo "El usuario es: ";
+		echo $reg['id_usuario'];
+		$id = $reg['id_usuario'];
+
+		//Creo una variable con ese ID
+		session_start();
+		$_SESSION['id'] = $id;
+
+		//redirecciono al user a la pagina interna
+		header("location: interno.php");
+
+		} else {
+		
+			echo "Los datos son incorrectos";
+	}
+
+}
+
+
+
 ?>
